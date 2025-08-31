7d109b5d7860a01d9002a16729703d5945f5bdee
Shuko <38896735+gonzalo1997@users.noreply.github.com>
2025-08-31 19:36:14 -0300

    login user with wrong credentials test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test.describe('Smoke Test - Login', () => {
-  test('Debería loguearse correctamente con credenciales válidas', async ({ page }) => {
+test.describe('Login User with correct email and password', () => {
+  test('Wrong credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login('gonzalo123@gmail.com', 'gonzalo123');
+
+    // Validación básica: redirección o mensaje visible
+    //await expect(page).toHaveURL(/\/products/); // o la URL que aparece post-login
+    await expect(page.getByText('Logged in as')).toBeVisible(); // texto visible tras login
+  });
+});
+
+test.describe('Login User with incorrect email and password', () => {
+  test('', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.navigate();
